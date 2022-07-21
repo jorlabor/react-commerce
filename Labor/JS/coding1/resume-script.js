b770f4a603c164dc8f57efbfe1fbd19ff185a702
unknown <you@example.com>
2022-07-21 10:53:17 +0800

    JS Exercise 1 files

--- Labor/JS/coding1/resume-script.js
+++ Labor/JS/coding1/resume-script.js
@@ -2,14 +2,28 @@
 // let body = document.querySelector("body");
 // body.style.backgroundColor = '#000000';
 // console.log("Body", body);
-document.addEventListener("click", ()=>{
-    let body = document.querySelector("body");
-    body.style.backgroundColor = '#000000';
-})
+// document.addEventListener("click", ()=>{
+//     let body = document.querySelector("body");
+//     body.style.backgroundColor = '#000000';
+// })
 
-let h3 = document.querySelectorAll("h3");
+let section = document.querySelector(".header-address");
 
-h3.addEventListener("hover", ()=>{
-    // let h3 = document.querySelector("h3");
-    h3.style.color = "#FFFFFF";
+section.addEventListener("mouseenter", ()=>{
+    
+    section.classList.add("header-address-color");
 });
+
+section.addEventListener("mouseleave", ()=>{
+    alert("ENTER");
+    section.classList.remove("header-address-color");
+});
+
+const buttonA = document.querySelector('#button_A');
+const headingA = document.querySelector('#heading_A');
+
+buttonA.onclick = () => {
+    const name = prompt('What is your name?');
+    alert(`Hello ${name}, nice to see you!`);
+    headingA.textContent = `Welcome ${name}`;
+}
