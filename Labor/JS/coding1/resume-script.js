cb5edb49faa2f2215ef4907925344659f26a8b3f
unknown <you@example.com>
2022-07-21 10:00:17 +0800

    first exercise js

---
+++ Labor/JS/coding1/resume-script.js
@@ -0,0 +1,15 @@
+
+// let body = document.querySelector("body");
+// body.style.backgroundColor = '#000000';
+// console.log("Body", body);
+document.addEventListener("click", ()=>{
+    let body = document.querySelector("body");
+    body.style.backgroundColor = '#000000';
+})
+
+let h3 = document.querySelectorAll("h3");
+
+h3.addEventListener("hover", ()=>{
+    // let h3 = document.querySelector("h3");
+    h3.style.color = "#FFFFFF";
+});
