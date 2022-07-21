a2817a57620316b87983019f836732465c582fe8
unknown <you@example.com>
2022-07-21 10:55:06 +0800

    Moved the Exer1 files in the parent directory

--- Labor/JS/coding1/resume-script.js
+++ Labor/JS/coding1/resume-script.js
@@ -2,10 +2,10 @@
 // let body = document.querySelector("body");
 // body.style.backgroundColor = '#000000';
 // console.log("Body", body);
-// document.addEventListener("click", ()=>{
-//     let body = document.querySelector("body");
-//     body.style.backgroundColor = '#000000';
-// })
+document.addEventListener("click", ()=>{
+    let body = document.querySelector("body");
+    body.style.backgroundColor = '#000000';
+})
 
 let section = document.querySelector(".header-address");
 
@@ -18,12 +18,3 @@ section.addEventListener("mouseleave", ()=>{
     alert("ENTER");
     section.classList.remove("header-address-color");
 });
-
-const buttonA = document.querySelector('#button_A');
-const headingA = document.querySelector('#heading_A');
-
-buttonA.onclick = () => {
-    const name = prompt('What is your name?');
-    alert(`Hello ${name}, nice to see you!`);
-    headingA.textContent = `Welcome ${name}`;
-}
